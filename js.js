ea3bd8706ecc1a686e0bdb5734bc361eb2edc088
Abrahão Lyncon <abrahaolyncon12@gmail.com>
2021-10-12 21:00:55 -0400

    Codigos refatorados e com validação via JavaScript

--- js.js
+++ js.js
@@ -1,4 +1,9 @@
 function calculate(){
+    var input1 = document.getElementById("bill-amount");
+    var input2 = document.getElementById("tip-p");
+    if(input1.value.length <= 0 || input2.value.length <= 0 ){
+        alert("Preencha os campos ;)")
+    }else{
     var billAmount = Number(document.getElementById("bill-amount").value);
     var tipAmount  = Number(document.getElementById("tip-p").value);
     tipAmount = tipAmount / 100;
@@ -7,7 +12,8 @@ function calculate(){
     var tipHtml = "<h3>The total tip is: $ <small>" + totalTip.toFixed(2) + "</small></h3>";
     var billHtml = "<h3>Your Bill with Tip is: $ <small>" + totalBill.toFixed(2) + "</small></h3>";
     document.getElementById("res-tip").innerHTML = tipHtml
-    document.getElementById("res-conta").innerHTML = billHtml
+    document.getElementById("res-conta").innerHTML = billHtml 
+    }
 }
 
 
